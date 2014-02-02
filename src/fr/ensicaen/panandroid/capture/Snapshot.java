ad0ddf8d6c8d7ee1c105c279f27c96d98f47cef1
NicolasThierion <Nicolasthierion@hotmail.fr>
2014-02-02 16:44:27 +0100

    Added snapshot class

---
+++ src/fr/ensicaen/panandroid/capture/Snapshot.java
@@ -0,0 +1,58 @@
+package fr.ensicaen.panandroid.capture;
+
+import android.graphics.Bitmap;
+
+public class Snapshot
+{
+	Bitmap mBitmap;
+	float mYaw;
+	float mPitch;
+	//final float mRoll;
+	String mFileName;
+	
+	public Snapshot(float yaw, float pitch)
+	{
+		mYaw = yaw;
+		mPitch = pitch;
+	}
+	
+	public Snapshot()
+	{
+	}
+	
+	
+	
+	public Bitmap getBitmap() {
+		return mBitmap;
+	}
+	public void setBitmap(Bitmap mBitmap) {
+		this.mBitmap = mBitmap;
+	}
+	public float getYaw() {
+		return mYaw;
+	}
+	
+	public float getPitch() {
+		return mPitch;
+	}
+
+	public String getFileName() {
+		return mFileName;
+	}
+	
+	public void setFileName(String mFileName) {
+		this.mFileName = mFileName;
+	}
+	
+	
+	public void setPitch(float pitch)
+	{
+		mPitch = pitch;
+	}
+	
+	public void setYaw(float yaw)
+	{
+		mYaw = yaw;
+	}
+	
+}
