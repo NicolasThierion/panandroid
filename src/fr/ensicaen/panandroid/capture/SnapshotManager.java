e97530aa151cf5d0b293fc889f1fc5337f836dbb
saloua <saloua.benseddik@ecole.ensicaen.fr>
2014-02-03 21:19:38 +0100

    created a JSON File that contains snapshots with their pitch and yaw values

--- src/fr/ensicaen/panandroid/capture/SnapshotManager.java
+++ src/fr/ensicaen/panandroid/capture/SnapshotManager.java
@@ -1,11 +1,21 @@
 package fr.ensicaen.panandroid.capture;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.opencv.core.Mat;
+
+import android.os.Environment;
+
 public class SnapshotManager 
 {
-	List<Snapshot> mSnapshots;
+	private List<Snapshot> mSnapshots;
 	
 	
 	
@@ -26,4 +36,65 @@ public class SnapshotManager
 		mSnapshots.add(snapshot);
 		
 	}
+	
+	public boolean createJSONFile()
+	{
+		String panoName = "panoname";
+		String panoData;
+		
+		JSONObject snapshots = new JSONObject();
+		
+		try {
+			snapshots.put("panoName", panoName);
+	
+		
+			JSONArray list = new JSONArray();
+			
+			for( int i = 0; i < mSnapshots.size(); i++)
+			{
+				JSONObject snapshot = new JSONObject();
+				Snapshot currentSnapshot = mSnapshots.get(i);
+				snapshot.put("snapshotId", currentSnapshot.getId());
+				
+				snapshot.put("urlName", currentSnapshot.getFileName());
+				snapshot.put("pitch", currentSnapshot.getPitch());	
+				snapshot.put("yaw", currentSnapshot.getYaw());
+				
+				list.put(snapshot);
+			}
+	 
+			snapshots.put("panoData", list);
+		 
+			File directory = new File(Environment.getExternalStorageDirectory()
+		                + File.separator + "Panandroid");
+	
+			if (!directory.exists()) {
+				directory.mkdirs();
+			}
+			try
+			{
+		 
+				FileWriter file = new FileWriter(directory.getAbsolutePath()+File.separator+panoName+".json");
+				file.write(snapshots.toString());
+				file.flush();
+				file.close();
+		 
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+				return false;
+			}
+		} catch (JSONException e1) {
+			e1.printStackTrace();
+			return false;
+		}
+		
+		return true; 
+	}
+	
 }
+
+	
+	
+	
+
